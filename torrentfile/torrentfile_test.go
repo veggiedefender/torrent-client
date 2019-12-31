acd4fe9c62a9fd4f43b158e3242b5cdb2a7d827a
Jesse Li <jessejesse123@gmail.com>
2019-12-31 12:18:18 -0500

    Implement golden file test for torrentfile.Open

--- torrentfile/torrentfile_test.go
+++ torrentfile/torrentfile_test.go
@@ -1,10 +1,40 @@
 package torrentfile
 
 import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+var update = flag.Bool("update", false, "update .golden.json files")
+
+func TestOpen(t *testing.T) {
+	input, err := os.Open("testdata/archlinux-2019.12.01-x86_64.iso.torrent")
+	require.Nil(t, err)
+
+	torrent, err := Open(input)
+	require.Nil(t, err)
+
+	goldenPath := "testdata/archlinux-2019.12.01-x86_64.iso.torrent.golden.json"
+	if *update {
+		serialized, err := json.MarshalIndent(torrent, "", "  ")
+		require.Nil(t, err)
+		ioutil.WriteFile(goldenPath, serialized, 0644)
+	}
+
+	expected := &TorrentFile{}
+	golden, err := ioutil.ReadFile(goldenPath)
+	require.Nil(t, err)
+	err = json.Unmarshal(golden, expected)
+	require.Nil(t, err)
+
+	assert.Equal(t, expected, torrent)
+}
 
 func TestToTorrent(t *testing.T) {
 	tests := map[string]struct {
